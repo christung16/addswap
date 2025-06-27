fa145d18157aba7e8c30fa560b6ad3686c1fff5c
christung16 <yitung@cisco.com>
2025-06-27 23:20:23 +0800

    Update add_swap.sh
    change bs=1024, count=4M

--- add_swap.sh
+++ add_swap.sh
@@ -2,7 +2,7 @@
 
 swapoff -a
 echo "It takes a few minutes, be patient!!"
-dd if=/dev/zero of=/mnt/swap bs=1024 count=4G
+dd if=/dev/zero of=/mnt/swap bs=1024 count=2G
 chmod 0600 /mnt/swap
 mkswap /mnt/swap
 swapon /mnt/swap
